34811865a703515585f136f177aa708c3a8ba91e
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2022-12-19 00:39:00 +0800

    Resolved potential problems

--- src/main/java/io/github/anvilloystudio/minimods/mod/core/ores/mixins/StackableItemMixin.java
+++ src/main/java/io/github/anvilloystudio/minimods/mod/core/ores/mixins/StackableItemMixin.java
@@ -1,8 +1,9 @@
 package io.github.anvilloystudio.minimods.mod.core.ores.mixins;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
+import io.github.anvilloystudio.minimods.api.GraphicComp;
+import minicraft.gfx.Sprite;
+import minicraft.item.Item;
+import minicraft.item.StackableItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,10 +12,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import io.github.anvilloystudio.minimods.api.GraphicComp;
-import minicraft.gfx.Sprite;
-import minicraft.item.Item;
-import minicraft.item.StackableItem;
+import java.io.IOException;
+import java.util.ArrayList;
 
 @Mixin(StackableItem.class)
 public class StackableItemMixin {
@@ -27,25 +26,25 @@ public class StackableItemMixin {
 	private static void injectMoreItems(CallbackInfoReturnable<ArrayList<Item>> ci, ArrayList<Item> items) {
 		try {
 			items.add(invokeInit("Copper Ore", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/copper_ore.png"))))); // Copper Ore
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/copper_ore.png"))))); // Copper Ore
 			items.add(invokeInit("Copper Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/copper_ingot.png"))))); // Copper Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/copper_ingot.png"))))); // Copper Ingot
 			items.add(invokeInit("Tin Ore", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/tin_ore.png"))))); // Tin Ore
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/tin_ore.png"))))); // Tin Ore
 			items.add(invokeInit("Tin Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/tin_ingot.png"))))); // Tin Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/tin_ingot.png"))))); // Tin Ingot
 			items.add(invokeInit("Lead Ore", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/lead_ore.png"))))); // Lead Ore
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/lead_ore.png"))))); // Lead Ore
 			items.add(invokeInit("Lead Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/lead_ingot.png"))))); // Lead Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/lead_ingot.png"))))); // Lead Ingot
 			items.add(invokeInit("Silver Ore", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/silver_ore.png"))))); // Silver Ore
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/silver_ore.png"))))); // Silver Ore
 			items.add(invokeInit("Silver Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/silver_ingot.png"))))); // Silver Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/silver_ingot.png"))))); // Silver Ingot
 			items.add(invokeInit("Bronze Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/bronze_ingot.png"))))); // Bronze Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/bronze_ingot.png"))))); // Bronze Ingot
 			items.add(invokeInit("Steel Ingot", GraphicComp.getSpriteFromSheet(1, 1,
-				GraphicComp.getSpriteSheetFromInputStream(StackableItem.class.getResourceAsStream("/assets/textures/items/steel_ingot.png"))))); // Steel Ingot
+				GraphicComp.getSpriteSheetFromInputStream(StackableItemMixin.class.getResourceAsStream("/assets/textures/items/steel_ingot.png"))))); // Steel Ingot
 		} catch (IOException e) {
 			throw new RuntimeException("Unable to initialize item(s).", e);
 		}
